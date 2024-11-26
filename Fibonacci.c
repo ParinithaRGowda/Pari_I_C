509f56972758776264069dc9f81e57637c00376a
Parinitha R Gowda <parinithargowda5@gmail.com>
2024-11-26 03:06:42 -0800

    Add files via upload

---
+++ Fibonacci.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+  int n, t1 = 0, t2 = 1, nextTerm = 0;
+  printf("Enter the number of terms: ");
+  scanf("%d", &n);
+  printf("Fibonacci Series: %d, %d, ", t1, t2); {
+  nextTerm = t1 + t2;
+  printf("%d, ", t1);
+  t1 = t2;
+}
+}
+  
+    
