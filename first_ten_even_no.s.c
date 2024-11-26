abd5859048d5e3eb7ad85b29749ee9c629f53146
Parinitha R Gowda <parinithargowda5@gmail.com>
2024-11-26 06:41:13 -0800

    Add files via upload

---
+++ first_ten_even_no.s.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int i = 1, count = 0;
+    while (count < 10) {
+        if (i % 2 == 0) {
+            printf("%d ", i);
+            count++;
+        }
+        i++;
+    }
+    return 0;
+}
